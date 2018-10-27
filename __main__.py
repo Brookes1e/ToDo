472b062130612ede75bd8bb30655fd32e934e617
Brookes1e <harrybrookes@hotmail.co.uk>
2018-10-27 22:55:04 +0100

    Increased functionality to allow adding and removing of tasks from live db which can be saved/loaded via pickle

--- __main__.py
+++ __main__.py
@@ -1,28 +1,89 @@
-#!/bin/python
+#!/usr/bin/python
 
 import argparse
 import datetime
-import pandas
 import subprocess
+import pickle
+import os.path
+
+storageFile = "./tasks.pkl"
 
 
 def main():
-    cliparser = argparse.ArgumentParser()
-    cliparser.add_argument("--add", "-a", help="Add a task to your ToDo list")
-    cliparser.add_argument("--delete", "-d", help="Remove a task from your ToDo list")
-    cliparser.add_argument("--reminder", "-r", help="Set a reminder for your task")
+    taskAction = argparse.ArgumentParser(description="Select an action for Todo")
+#    taskAction.add_mutually_exclusive_group(required=True)
+    taskAction.add_argument("--add", "-a", \
+            help="Add a task to your ToDo list")
+    taskAction.add_argument("--delete", "-d", \
+            help="Remove a task from your ToDo list")
+    taskAction.add_argument("--reminder", "-r", \
+            help="Set a reminder for your task")
+    taskAction.add_argument("--list", "-l", \
+            help="Set a reminder for your task", default=False, action="store_true")
+    args = taskAction.parse_args()
+    
+    task = Task(args)
+    managetasks = ManageTasks(task)
+    
+    #subprocess.check_output(['systemctl', 'restart', 'todo-timer.timer']) 
+    
+class ManageTasks():
+    def __init__(self,task):
+        self.db = []
+        self.opendb()
+        print self.db
+        self.task = task
+        self.actionSwitch()
 
-    args = cliparser.parse_args()
+	
+    def opendb(self):
+        if os.path.isfile(storageFile):
+            with open(storageFile, "rb") as handle: 
+                self.db = pickle.load(handle)
+
+    def savedb(self):
+        with open(storageFile, "wb") as handle: 
+            pickle.dump(self.db, handle)
+        
+    def actionSwitch(self):
+        if self.task.add != None:
+            self.new()
+        if self.task.delete != None:
+            self.delete()
+        if self.task.list != None:
+            self.list()
+
+    def new(self):
+        self.db.append(self.task)
+        self.savedb()
     
-    subprocess.check_output(['systemctl', 'restart', 'todo-timer.timer']) 
-    
+    def delete(self):
+        for index,delTask in enumerate(self.db):
+            print delTask.add
+            if delTask.add == self.task.delete:
+                del self.db[index]
+
+    def edit(self):
+        pass
+
+    def list(self):
+    	print [listTask.add for listTask in self.db]
+
+    def __del__(self):
+        self.savedb()
+        del self.db
+        del self.task
+
+
 
 class Task():
-    def __init__(self):
+    def __init__(self,classargs):
         self.create_time = datetime.datetime.time(datetime.datetime.now())
-        self.subject = args.add
-        self.reminder = args.reminder
-
+        self.add = classargs.add
+        self.delete = classargs.delete
+        self.reminder = classargs.reminder
+        self.list = classargs.list
 
 if __name__ == "__main__":
     main()
+
