f810d07b0bc5debdc7392f8185ce16e5039ba66b
Brookes1e <harrybrookes@hotmail.co.uk>
2018-11-04 22:01:52 +0000

    1. Basic working outline of the goodle driver wrapper. 2. Updated .gitignore to hide .tasks.pkl.old

--- google-wrapper.py
+++ google-wrapper.py
@@ -4,7 +4,11 @@
 from googleapiclient.discovery import build
 from httplib2 import Http
 from oauth2client import file, client, tools
-from apiclient.http import MediaFileUpload
+from apiclient.http import MediaFileUpload, MediaIoBaseDownload
+from datetime import datetime
+import time
+from os import path, mknod, utime, rename
+import io
 
 # If modifying these scopes, delete the file token.json.
 SCOPES = 'https://www.googleapis.com/auth/drive'
@@ -13,48 +17,60 @@ class DriveWrapper():
     def __init__(self,service):
         self.saved = False
         self.service = service
-        self.folder = 'ToDo'
         self.pickleFile = 'tasks.pkl'
-        self.folderMetadata = {
-            'name': self.folder,
-            'mimeType': 'application/vnd.google-apps.folder',
-        }
-        self.fileMetadata = {
-            'name': 'task.pkl'
-        }
+        self.fileName = 'tasks.pkl'
+        self.checkFile()
 
-
-    def checkDrive(self):
-        gFolder = self.service.files()\
-                .list(q="name='ToDo' and mimeType='application/vnd.google-apps.folder'")\
-                .execute()\
-                .get('files', [])
-        
-        if not gFolder:   
-            self.createFolder()
+    def checkFile(self):
+        self.file = self.service.files().list(q="name='%s'" % self.fileName).execute()
+        print self.file
+        if not self.file.get('files'):
+            self.createFile()
         else:
-            self.folderMetadata['id'] = gFolder[0].get('id')
-            print "The ToDo folder already exists on this GDrive"
-    
-    def createFolder(self):
-        todoFolder = self.service.files().create(body=self.folderMetadata)
-    
-    def saveFile(self):
+            self.id =  self.file['files'][0].get('id')
+        self.syncFile()
+
+    def createFile(self):
         media = MediaFileUpload('tasks.pkl')
-        todoFile = self.service.files().create(body=self.fileMetadata,
+        todoFile = self.service.files().create(body={
+                                                    'name' : 'tasks.pkl'
+                                                },
                                                media_body=media,
                                                fields='id').execute()
-        self.service.parents().insert(body=self.folderMetadata.get('id'),
-                                               file=todoFile.get('id'),
-                                               fields='id').execute()
+        self.id = todoFile.get('id')
 
-        print todoFile.get('id')
+    def lastRemoteUpdateEpoc(self):
+         lastRemoteUpdate = str(datetime.strptime(self.service.files().get(fileId=self.id, fields='modifiedTime').execute().get('modifiedTime').replace('T','')[:-5], '%Y-%m-%d%H:%M:%S'))
+         return int(time.mktime(time.strptime(lastRemoteUpdate, '%Y-%m-%d %H:%M:%S')))
 
-    def syncList(self):
-        pass
+    def lastLocalUpdateEpoc(self):
+        try:
+            return int(path.getctime(self.fileName))
+        except OSError:
+            return 0
 
-    def removeFolder(self):
-        pass
+
+    def syncFile(self):
+        if self.lastRemoteUpdateEpoc() > self.lastLocalUpdateEpoc():
+            print "Remote file is ahead of local cache, updating local cache..."
+            rename(self.fileName,'.'+self.fileName+'.old')
+            self.pull()
+        else:
+            self.push()
+
+    def push(self):
+        print "pushed"
+        self.service.files().update(fileId=self.id, media_body=MediaFileUpload('tasks.pkl')).execute()
+        utime(self.fileName,None)
+
+    def pull(self):
+        print "pulled"
+        done = False
+        downloader = MediaIoBaseDownload(io.FileIO('tasks.pkl','wb'), self.service.files().get_media(fileId=self.id))
+        while done is False:
+            status, done = downloader.next_chunk()
+        self.service.files().update(fileId=self.id).execute()
+
 
 def main():
     """Shows basic usage of the Drive v3 API.
@@ -70,8 +86,6 @@ def main():
     # Call the Drive v3 API
     
     wrapper = DriveWrapper(service)
-    wrapper.checkDrive()
-    wrapper.saveFile()
     
     #results = service.files().list(
     #    pageSize=10, fields="nextPageToken, files(id, name)").execute()
